9442f5b1c5235e5115a766c296aa0d680fa4c2cd
bug_101 <chemerilcollins@gmail.com>
2023-01-20 17:04:55 +0100

    I modified the cart

--- DOM.js
+++ DOM.js
@@ -1,2 +1,33 @@
+console.clear()
 let ul = document.querySelector('ul')
-console.log(ul);
+console.log(ul.parentElement);
+console.log(ul.parentNode);
+
+//To get to the parent element we use 
+/**
+ * .parentElement
+ * .parentNode
+ * .parentNode.parentNode - grand parent
+ */
+console.log(document.documentElement); //logs to the console the root of the document
+console.log(ul.childNodes[1])//this returns even text nodes and comment nodes
+console.log(ul.firstElementChild);
+/*
+Those that return even text nodes and comments:
+
+childNodes
+firstChild
+lastChild
+nextSibling
+previousSibling
+
+Those that don't return text nodes and comments:
+
+children
+firstElementChild
+lastElementChild
+nextElementSibling
+previousElementSibling
+*/
+
+//EVENT LISTENERS
