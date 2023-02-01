240432f71f43fd2bd369ae5ee1eb30640ed2a644
bug_101 <chemerilcollins@gmail.com>
2023-02-01 12:46:37 +0100

    Changed the images directories and in code

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ let lgreen = document.querySelector(".lgreen")
 let red = document.querySelector(".red")
 let blue = document.querySelector(".blue")
 function bl() {
-    div2.style.background = 'url(./photos/blue.png)'
+    div2.style.background = 'url(blue.png)'
     div2.style.backgroundRepeat = 'no-repeat'
     div2.style.backgroundSize = 'contain'
 }
 function r() {
-    div2.style.background = 'url(./photos/red.png)'
+    div2.style.background = 'url(red.png)'
     div2.style.backgroundRepeat = 'no-repeat'
     div2.style.backgroundSize = 'contain'
 }
 function lg() {
-    div2.style.background = 'url(./photos/lgreen.png)'
+    div2.style.background = 'url(lgreen.png)'
     div2.style.backgroundRepeat = 'no-repeat'
     div2.style.backgroundSize = 'contain'
 }
